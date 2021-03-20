0a8d95c41aa72d8ece051f97c3207363cf5849d3
Edward Shirinian <edshirinian@gmail.com>
2021-03-20 17:34:31 -0400

    Added GalleryActivity class which is where our  gallery will be. Left MainActivity blank with just a button to access GalleryActivity. I assume we'll use MainActivity as the setup page.

--- app/src/main/java/com/example/querygallery/MainActivity.java
+++ app/src/main/java/com/example/querygallery/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.querygallery;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.lang.reflect.GenericArrayType;
 
 public class MainActivity extends AppCompatActivity {
+
+    //This activity is intentionally left empty for now
+    /*
+     * RecyclerView is a type of View that recycles a position in your View. ex when an item scrolls off screen
+     * RecyclerView does not destroy its view. Instead RecyclerView  reuses the view for new items that have
+     * scrolled on screen.
+     */
+    RecyclerView folderRecycler;
+    TextView empty;
+
+    private static final int EXTERNAL_STORAGE_REQUEST_PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void switchActivity(View v){
+        Intent intent = new Intent(this, GalleryActivity.class);
+        startActivity(intent);
     }
 }
